4aa35f301519e6cc887353cd4f489842c64dc282
ghatikawaykole <ghatika99@gmail.com>
2021-11-02 02:04:19 +0530

    Added addressbook data in the compute

--- addressbook.js
+++ addressbook.js
@@ -1,6 +1,6 @@
 class addressbook{
-    constrcutor(firstName,lastName,address,city,state,zip,phone,email){
-        this.firtsName = params[0];
+    constructor(...params){
+        this.firstName = params[0];
         this.lastName = params[1];
         this.address = params[2];
         this.city = params[3];
@@ -11,52 +11,77 @@ class addressbook{
     }
     get firstName() { return this._firstName; }
     set firstName(firstName) {
-    
+        let nameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+        if (nameRegex.test(firstName))
             this._firstName = firstName;
+        else
+            throw "Invalid first Name";
     }
 
     get lastName() { return this._lastName; }
     set lastName(lastName) {
+        let nameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+        if (nameRegex.test(lastName))
             this._lastName = lastName;
+        else
+            throw "Invalid  last Name";
     
     }
     get address() { return this._address; }
     set address(address) {
-
+        let addressRegex = RegExp("^[A-Za-z]{4,}$");
+        if (addressRegex.test(address))
             this._address = address;
+        else
+            throw "Invalid address ";
     }
 
 
     get city() { return this._city; }
     set city(city) {
-
+        let cityRegex = RegExp("^[A-Za-z]{4,}$");
+        if (cityRegex.test(city))
             this._city = city;
-       
+        else
+            throw "Invalid city ";
     }
 
     
     get state() { return this._state; }
     set state(state) {
-                    this._state = state;
-    
+        let stateRegex = RegExp("^[A-Za-z0-9]{4,}$");
+        if (stateRegex.test(state))
+            this._state = state;
+        else
+            throw "Invalid state";
     }
 
     get zip() { return this._zip; }
     set zip(zip) {
-    
+        let zipRegex = RegExp("^[0-9]{3}[ ]*[0-9]{3}$");
+        if (zipRegex.test(zip))
+            this._zip = zip;
+        else
+            throw "Invalid zip ";
             this._zip = zip;
     }
 
-    get phoneNumber() { return this._phoneNumber; }
-    set phoneNumber(phoneNumber) {
-            this._phoneNumber = phoneNumber;
+    get phone() { return this._phone; }
+    set phone(phone) {
+        let phoneRegex = RegExp("^[1-9]{2}[ ]{1}[0-9]{10}$");
+        if (phoneRegex.test(phone))
+            this._phone = phone;
+        else
+            throw "Invalid phone number";
     }
 
     get email() { return this._email; }
     set email(email) {
-    
+        let emailRegex = RegExp("^([a-z0-9A-Z])+([.]?[a-z0-9A-Z]+)*[@]{1}[a-z0-9A-Z]+[.]{1}[a-zA-Z]{2,}([.]{1}[a-z]{2,})?$");
+        if (emailRegex.test(email))
             this._email = email;
-    
+        else
+            throw "Invalid email";
     }
 
     toString() {
@@ -65,14 +90,12 @@ class addressbook{
             "\nCity: " + this.city +
             "\nState: " + this.state +
             "\nZip: " + this.zip +
-            "\nPhone Number: " + this.phoneNumber +
+            "\nPhone Number: " + this.phone +
             "\nEmail: " + this.email;
     }
 }
 let contactarray = new Array();
-contactarray.push(new addressbook("mayura","kolhe","sarasvatinagar","jalgoan","maharashtra",1234,1111111111,"mayuraa@gmail.com"));
+contactarray.push(new addressbook("Mayura","Kolhe","sarasvatinagar","jalgoan","maharashtra",123456,"11 1111111111","mayuraa@gmail.com"));
 console.log(contactarray);
 let length = contactarray.length;
-for(let i=0;i<length;i++){
-console.log(contactarray(i));
-}
+contactarray.forEach((contact) => console.log(contact.toString()));
